60b46484af1a3f5162fb363fbfe36b0680b1107c
yujinchoe <dev.yujinchoe@gmail.com>
2023-09-30 06:03:52 +0900

    feat: 네비바 날짜 이동 구현

--- petLifeLog/Controllers/Home/HomeViewController.swift
+++ petLifeLog/Controllers/Home/HomeViewController.swift
@@ -9,73 +9,83 @@ import UIKit
 
 //테스트용 임시 데이터
 let user_id = "admin" // 로그인 후 들고 있어야 할 user_id값
-let pet_id = "adminpet" // 로그인 후 들고 있어야 할 user_id의 현재 선택 되어있는 pet_id의 값
-
-let today = Date() // 2023-09-29 17:05:34 +0000
+let pet_id = "adminpet" // 로그인 후 들고 있어야 할 user_id의 현재 선택 되어있는 pet_id의 값(pet_id로 강아지생일필요)
 
 //var dataset // 전역변수로 선언 미리 해주기
 //임시데이터
-var dataset = [["image":"twinlake", "act_name":"배변", "actmemo":"nothing special", "pootype":"초코/정상", "photo":"twinlake"],["image":"twinlake", "act_name":"배변", "actmemo":"nothing special", "pootype":"초코/정상", "photo":"twinlake"],["image":"twinlake", "act_name":"배변", "actmemo":"nothing special", "pootype":"초코/정상", "photo":"twinlake"]]
+var dataset = ["pet1","pet2","pet3","pet4"]
 
 class HomeViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var currentDate: UINavigationItem! // 오늘 날짜 지정하기 위해서
-    var selected_date = "" // 네비바용. 사용자가 선택한 날짜. yyyy-mm-dd
+    @IBOutlet weak var nextBtn: UIBarButtonItem!
     
+    var selected_date = "" // 네비바용. 사용자가 선택한 날짜. yyyy-MM-dd
+    var today = "" // < > 날짜 이동 버튼, 날짜에 따라 비활성화 처리용 대조 데이터
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTableView()
-        getDataset() // 테이블뷰 액션 셀의 row들에 보여줄 액션명, 다이어리 셀에 보여줄 다이어리 데이터 가져오기
-        //getDateTime() 날짜 선택 화면이동 데이터 플로우에 따라.. will에 보일지 did에 보일지 생각하기...
-        getDateTime()
+        let now = Int(Date().timeIntervalSince1970) // unixTime. 1972년 1월 1일부터로부터 몇초가 경과했는지
+        setDate(now)
+        nextBtn.isEnabled = false // 오늘 다음 날짜의 기록은 없으니 처음엔 > 버튼 비활성화로 시작하기
+        today = selected_date // nextDate버튼 비활성화 처리 위하여 오늘 날짜와 비교하기 위해서 받아두기
+        getDataset() // 세팅된 날짜의 테이블뷰 액션 셀의 row들에 보여줄 액션명, 다이어리 셀에 보여줄 다이어리 데이터 가져오기
+        setupTableView() // 날짜 맞게 테이블뷰 셋업
     }
     
-    // TODO: 날짜 연산. 달의 마지막날+1일 할 경우 다음달 1일으로 가야, 1일은 0일이 아닌 이전달 마지막날으로 가야. 고려해서 로직 다시 생각하기.
-    //let dateFormatter = DateFormatter()
-    // let convertDate = dateFormatter.date(from: dateStr) // Date 타입으로 변환
-    //
+
     //날짜 -1 버튼(<)
     @IBAction func prevDate(_ sender: Any) {
-        // yyyy-mm-dd -> yyyy-mm-(dd-1)
-        let dd = (Int(selected_date.suffix(2)) ?? 0) - 1 //dd - 1
-        let yyyymm = String(selected_date.prefix(8)) //yyyy-mm-
-        let prevDate = yyyymm + String(dd)
-        print(prevDate)
-        let dateFormatter = DateFormatter()
-        let convertPrevDate = dateFormatter.date(from: prevDate)
-        print(convertPrevDate ?? 0)
-        // currentDate.title =
+        // TODO: 강아지 생일 날짜 이전 날짜의 버튼은 비활성화 되도록 하기.
+        
+        nextBtn.isEnabled = true // 오늘 이전 날짜로 가면 다음 날짜로 갈 수 있으니 next 활성화 시키기
+        
+        let navidate = selected_date.toDate() //yyyy-MM-dd 스트링을 Date타입으로 변환
+        let navidatenum = navidate!.timeIntervalSince1970 // Date타입을 unixTime으로 변환
+    
+        let prevDay = Int(navidatenum) - 86400 // unixTime을 Int로 변환하여 하루(86400) 빼기
+        setDate(prevDay)
+        
+        // if selected_date == 강아지생일 {prevBtn.isEnabled = false } // 생일 이전은 기록 못하게. 생일 가져오기
+        
+        //테이블뷰 데이터도 바뀐 selected_date 날짜의 데이터로 불러오기
+        //getDataset()
+        //setupTableView() // 날짜 맞게 테이블뷰 셋업
     }
     
     //날짜 +1 버튼(>)
     @IBAction func nextDate(_ sender: Any) {
-        // yyyy-mm-dd -> yyyy-mm-(dd+1)
-        let dd = (Int(selected_date.suffix(2)) ?? 0) + 1 //dd + 1
-        let yyyymm = (selected_date.prefix(8)) //yyyy-mm-
-        let nextDate = yyyymm + String(dd)
-        print(nextDate)
-    }
-    
-    func getDateTime() {
-        let formatter = DateFormatter()
+        let navidate = selected_date.toDate()
+        let navidatenum = navidate!.timeIntervalSince1970
         
-        // 네비바 타이틀 설정용 날짜
-        formatter.dateFormat = "yyyy-MM-dd"
-        selected_date = formatter.string(from: Date())
-        currentDate.title = selected_date
+        let nextDay = Int(navidatenum) + 86400
+        setDate(nextDay)
+        
+        if selected_date == today { // 오늘 날짜에서는 다음 날짜 선택 버튼 비활성화되게. 미래 행동 기록 막기.
+            nextBtn.isEnabled = false }
+        
+        //테이블뷰 데이터도 바뀐 selected_date 날짜의 데이터로 불러오기
+        //getDataset()
+        //setupTableView() // 날짜 맞게 테이블뷰 셋업
     }
     
+    // MARK: 날짜 연산 -> 문자열로 변환 -> 전역변수 날짜 업데이트 -> 네비바에 해당 날짜 표시
+    func setDate(_ date: Int) {
+        let timeInterval = TimeInterval(date) // Int를 unixTime으로 변환
+        let changedDate = Date(timeIntervalSince1970: timeInterval) // unixTime을 Date타입으로 변환
+        selected_date = changedDate.toString() // Date를 스트링으로 변환 후 selected_date갱신.갱신된 날짜에서 재연산하여야하니까 최종 변환값을 대입해줘야함.
+        currentDate.title = selected_date // 네비바에 갱신된 날짜 보여주기
+    }
     
     // 메인 네비바상의 선택된 날짜에 현재 접속된 유저의 선택된 강아지의 기록(액션,일기)이 있다면 가져와라.
     func getDataset() {
         // TODO: 메인화면 테이블뷰 액션 셀의 row들에 보여줄 액션명, 다이어리 셀에 보여줄 다이어리 데이터 가져오기. pet_act
-        // API 통신 통해 pet_act테이블에서 selected_date==act_date 이면서 user_id==user_id, pet_id==pet_id에 일치하는 데이터를 1개 혹은 여러개 받는다.
+        // API 통신 통해 pet_act테이블에서 act_date==selected_date 이면서 user_id==user_id, pet_id==pet_id에 일치하는 데이터를 1개 혹은 여러개 받는다.
         //pet_act(id,[act_date],[diary_content],[diary_image],diary_open_yn,reg_datetime,[act_id],pet_id,user_id) 이 중 4개 필요함
-            // pet_act테이블에서 act_date와 diary_content와 diary_image와 act_id를 가져온다.
-                // act_id가 있으면 cell 1에 한 행씩 act_date와 act_id를 보여주고,
-                // act_id가 없으면(==diary라면) cell 2에 act_date와 diary_content와 diary_image를 보여준다.
+        // pet_act테이블에서 act_date와 diary_content와 diary_image와 act_id를 가져온다.
+        // act_id가 있으면 cell 1에 한 행씩 act_date와 act_id를 보여주고,
+        // act_id가 없으면(==diary라면) cell 2에 act_date와 diary_content와 diary_image를 보여준다.
         
         // TODO: 메인화면 테이블뷰 액션셀의 row들에 보여줄 각 액션의 세부 데이터 가져오기. pet_act_detail
         // pet_act_detail(id, act_time, memo, memo_image, walk_spend_time, ordure_shape, ordure_color, feed_type, feed_amount, feed_name, hospital_type, hospital_name, hospital_doctor, hospital_cost, beauty_cost, weight, petact_id)
@@ -100,6 +110,15 @@ class HomeViewController: UIViewController {
             let targetVC = segue.destination as! AddPetActionViewController
             targetVC.selectedDate = selected_date
         }
+        if segue.identifier == "toActionDetail" { // 액션 상세 편집 모달창으로 가는 segue
+            let targetVC = segue.destination as! PetDetailViewController
+            // targetVC.dataset = dataset // 상세페이지에서 기존 데이터 뿌려줘야 함. 네비바 날짜의 데이터로.
+        }
+        if segue.identifier == "toReadDiary" { // 다이어리 읽기 모달창으로 가는 segue
+            let targetVC = segue.destination as! AddDiaryViewController
+            // targetVC.dataset = dataset // 상세페이지에서 기존 데이터 뿌려줘야 함. 네비바 날짜의 데이터로.
+        }
+        
     }
 
 }
@@ -124,10 +143,10 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         var cell = UITableViewCell()
         if indexPath.section == 1 { // 일기 셀, 섹션 1
             cell = tableView.dequeueReusableCell(withIdentifier: "diaryCell", for: indexPath)
-            // cell tag활용 데이터 매칭
+            // cell tag활용 데이터 매칭하기
         } else { // 나머지 rows 전부 == 행동 기록 행들
             cell = tableView.dequeueReusableCell(withIdentifier: "actionCell", for: indexPath)
-            // cell tag활용 데이터 매칭
+            // cell tag활용 데이터 매칭하기
             
             
             // TODO: getDataset()으로 불러들여온 dataset을 꺼내서 diaryCell,actionCell의 각 컴포넌트에 매치해서 넣어준다.
@@ -145,9 +164,34 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
             //세부 디테일 내용은 메인셀에서 어디까지 보여줄것인가 정하기
             // pet_act_detail(id, act_time, memo, memo_image, walk_spend_time, ordure_shape, ordure_color, feed_type, feed_amount, feed_name, hospital_type, hospital_name, hospital_doctor, hospital_cost, beauty_cost, weight, petact_id)
             
-            
         }
         return cell
     }
 
+}
+
+
+// MARK: - 날짜 변환. 데이터 저장시 항상 고정된 타임존으로
+extension String {
+    func toDate() -> Date? { // db상 받아온 act_date의 문자타입 데이터를 날짜타입으로 변환하기 위한 용도
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.timeZone = TimeZone(identifier: "Asia/Seoul")
+        if let date = dateFormatter.date(from: self) {
+            return date
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Date {
+    func toString() -> String { // < > 버튼 누를시 날짜 연산하여 네비바에 날짜 포맷팅해서 입력하기 위한 용도
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.timeZone = TimeZone(identifier: "Asia/Seoul")
+        return dateFormatter.string(from: self)
+    }
 }
