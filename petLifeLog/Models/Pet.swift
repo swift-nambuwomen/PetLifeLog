4c19b51b96dc403bcaba7a909c5ddfd09a86ba1c
yujinchoe <dev.yujinchoe@gmail.com>
2023-10-11 15:48:31 +0900

    feat:Alamofire get method with body request 테스트

--- petLifeLog/Models/Pet.swift
+++ petLifeLog/Models/Pet.swift
@@ -38,3 +38,30 @@ struct PetActionDetail: Codable {
     var beauty_cost : Int?
     var weight : Double?
 }
+
+// MARK: - Actdetail
+struct Act: Codable {
+    let act:String?
+    let actdetail:[ActDetail]?
+    let act_date:String?
+    let diary_content:String?
+    let diary_image:String?
+    let diary_open_yn:String?
+    let pet:Int?
+    let user:Int?
+}
+
+struct ActDetail: Codable {
+    let id:Int?
+    let act_time:String?
+    let memo:String?
+    let memo_image:String?
+    let walk_spend_time:Int?
+    let ordure_shape:String?
+    let ordure_color:String?
+    let feed_type:String?
+    let feed_amount:Int?
+    let feed_name:String?
+    let hospital_type:String?
+    let weight:Int?
+}
